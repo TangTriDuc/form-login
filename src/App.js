205b41ec2dcd3a9d1b09582305bf61ae3b4ec210
TangTriDuc <19521382@gm.uit.edu.vn>
2022-10-21 14:03:52 +0700

    form login

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+import LoginForm from './components/LoginForm';
 
-function App() {
+function App() { 
+  const adminUser = {
+    email: "admin@admin.com",
+    password: "admin"
+  }
+
+  const [user, setUser] = useState({name:"",email:""});
+  const [error, setError] = useState("");
+
+
+  const Login = details => {
+    console.log(details);
+
+    if (details.email == adminUser.email && details.password == adminUser.password)
+      {console.log("Logged in");
+      setUser ({
+        name: details.name,
+        email: details.email
+      });
+      } else {
+        console.log("Do not match");
+        setError("Do not match");
+      }
+  }
+
+  const Logout = () => {
+    {/*console.log("Logout"); */}
+    setUser({name:"",email:""});
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {/* ? la render */}
+      {(user.email !="")? (
+        <div className="welcome">
+          <h2>Welcome, <span>{user.name}</span> </h2>
+          <button onClick={Logout}>Logout</button>
+        </div>
+      ) : (
+        <LoginForm  Login={Login} error={error}/>
+      )}
     </div>
   );
 }
